1dd5ca68abd8422ab1434de75aacac7372c4fed2
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-05-13 20:41:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,54 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  stuArr.map(varMap);
+
+    function varMap(value) {
+        if(value.marks >= 50) {
+            console.log(value);
+        }
+    }
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  stuArr.forEach(varForEach);
+
+    function varForEach(stuArr) {
+        if(stuArr.marks >= 50) {
+            console.log(stuArr);
+        }
+    }
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newObj = {id:4, name:"susan", age:"20", marks:45};
+
+    stuArr.push(newObj);
+    console.log(stuArr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  var filter_arr = stuArr.filter(function(fail) {
+        return fail.marks >= 50;
+    });
+
+    console.log(filter_arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const num = stuArr[stuArr.length-1].id ;
+    let sec_Arr = [
+        { id: num+1, name:"Rohit", age:"25", marks:Math.floor(Math.random() * 101)
+    }, 
+        { id: num+2, name:"Sanchit", age:"24", marks:Math.floor(Math.random() * 101)
+    }, 
+        { id: num+3, name:"Akarn", age:"24", marks:Math.floor(Math.random() * 101)
+    } 
+    ];
+    stuArr = stuArr.concat(sec_Arr);
+    console.log(stuArr);
 }
