451651f4d2bd79ad5f259a7cc11f3494b73b2811
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-05-13 20:48:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** @format */
 
-let arr = [
+let stuArr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
